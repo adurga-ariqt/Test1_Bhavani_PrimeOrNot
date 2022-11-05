ae00df7088c7ea63cc1005d5e1df1a53390e4ce5
adurga-ariqt <adurga@ariqt.com>
2022-11-05 16:17:38 +0530

    Add project files.

---
+++ Test1_Bhavani_PrimeOrNot/PrimeNumber1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Bhavani_PrimeOrNot
+{
+    public  class PrimeNumber1
+    {
+     public void PrimeNumberMethod()
+        {
+            int n , i;
+            Console.WriteLine("Enter number:");
+            n =Convert.ToInt32(Console.ReadLine());
+            int c = 0;
+            for (i = 1; i <= n; i++)
+            {
+                if (n / i == 0)
+                {
+                    c++;
+                }
+            }
+                if (c == 2)
+                {
+                    Console.WriteLine("given number is PrimeNumber");
+                }
+                else
+                {
+                    Console.WriteLine("The given number is not a primenumber");
+                }
+            
+
+        }
+    }
+}
---
+++ Test1_Bhavani_PrimeOrNot/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_Bhavani_PrimeOrNot;
+
+
+Console.WriteLine("Hello, World!");
+PrimeNumber1 obj = new PrimeNumber1();
+obj.PrimeNumberMethod();
